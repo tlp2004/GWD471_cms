e462dc8a602eee9b73ac10ab82ff1022e906c620
tlp2004 <pattersont2004@yahoo.com>
2014-05-11 14:56:46 -0400

    Added files
    Added css. header php and images

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header>
-        <h1>Welcome to Widgco Widgets</h1>
+        <h1>Support Women-Start-Ups</h1>
         <nav>
             <a href="index.php" class="home_link">Home</a>
             <a href="about.php" class="about_link">About</a>
